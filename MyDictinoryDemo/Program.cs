371a6ccb5fc08f6bfb7954a5236ff99f2a8267fe
begüm ünal <bgm.unl00@gmail.com>
2021-01-21 19:49:38 +0300

    MyDictionaryDemo

---
+++ MyDictinoryDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Begüm");
+            myDictionary.Add("Mert");
+            myDictionary.Add("Tuana");
+        }
+    }
+}
